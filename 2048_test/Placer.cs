a831e9d4f9f2ee6c4d05276597979f84d2000351
badsparky <92095206+badsparky@users.noreply.github.com>
2022-06-22 01:01:51 +0900

    done

---
+++ 2048_test/Placer.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class B22Placer : PlaceAgent
+{
+    public B22Placer(Random random) : base(random) {}
+
+    public override Tuple<Tuple<int, int>, int> place(List<GameField> history)
+    {
+        GameField latest = history[history.Count - 1];
+        Tuple<int, int>[] candidates = 
+        Enumerable.Range(0, latest.size.Item1).SelectMany(row => Enumerable.Range(0, latest.size.Item2), (first, second) => new Tuple<int, int>(first, second)).Where(x => latest.isValidPiecePlacement(x)).ToArray();
+
+        var answer = Montecarlo(candidates, latest);
+        
+        return new Tuple<Tuple<int, int>, int>(answer.Item1, answer.Item2);
+    }
+
+    public Tuple<Tuple<int, int>, int> Montecarlo(Tuple<int, int>[] candidates, GameField latest)
+    {
+        int depth = 100;
+        double[] score_array = new double[candidates.Length];
+        double min = 999999;
+        int ans_number = 0;
+        Tuple<int, int> ans_coordinate = candidates[0];
+        for(int i = 2; i <= 4; i += 2)
+        {
+            for(int j = 0; j < candidates.Length; j ++)
+            {
+                GameField test = new GameField(latest).putNewPieceAt(candidates[j], i);
+                for(int k = 0; k < depth; k++)
+                {
+                    while (!test.checkmate())
+                    {
+                        Challenge(test);
+                        score_array[j]++;
+                    }
+                    score_array[j] += Math.Log(test.maxpiece, 2);
+                }
+                if(min > score_array[j])
+                {
+                    ans_number = i;
+                    ans_coordinate = candidates[j];
+                    min = score_array[j];
+                }
+            }
+        }
+        var ans = new Tuple<Tuple<int, int>, int>(ans_coordinate, ans_number);
+
+        return ans;
+    }
+    public void Challenge(GameField test)
+    {
+        HashSet<string> candidates = new HashSet<string>();
+        foreach (var x in new string[] { "Up", "Down", "Left", "Right" }) { if (test.isValidSlide(x)) candidates.Add(x); }
+        string[] arrayed = candidates.ToArray();
+        string direction_random = arrayed[rand.Next(arrayed.Length)];
+        test.slide(direction_random);
+
+        Tuple<Tuple<int, int>, int> placement = Randomplace(test);
+
+        test.putNewPieceAt(placement.Item1, placement.Item2);
+
+        
+    }
+    public  Tuple<Tuple<int, int>, int> Randomplace(GameField gf)
+    {
+        GameField latest = gf;
+        Tuple<int, int>[] candidates =
+        Enumerable.Range(0, latest.size.Item1).SelectMany(row => Enumerable.Range(0, latest.size.Item2), (first, second) => new Tuple<int, int>(first, second)).Where(x => latest.isValidPiecePlacement(x)).ToArray();
+
+        return new Tuple<Tuple<int, int>, int>(candidates[rand.Next(candidates.Length)], rand.Next(5) == 0 ? 4 : 2);
+    }
+
+}
---
+++ 2048_test/main.cs
@@ -0,0 +1,112 @@
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Threading;
+
+public static class Program
+{
+    public static int[] Main_old(string[] args,Type type_solver)
+    {
+        int rand_seed = args.Length > 0 ? int.Parse(args[0]) : 0;
+        string log_path = args.Length > 1 ? args[1] : "";
+
+        Tuple<int, int> fieldsize = new Tuple<int, int>(4, 4);
+
+        GameController cont = new GameController(fieldsize, rand_seed);
+
+        cont.Run(typeof(B22Placer),type_solver, log_path : log_path );
+
+        LinkedList<GameField> history=cont.history;
+
+        int [] result=new int[2];
+        result[0] = history.Last.Value.maxpiece;
+        result[1] = cont.count;
+        return result;
+    }
+
+    public static void Main()
+    {
+        //*********************
+        int times =2;//10以上は、諸事情により10の倍数のみ
+        string path_result = "ResultFile.txt";
+        string path_log = "";
+        Type type_solver = typeof(B22Solver);//()内変更
+        //************************
+        DateTime start =DateTime.Now;
+        DateTime end;
+        Random rand;
+        int[] log_score = new int[times];
+        int[] log_count = new int[times];
+        string tmp = "";
+        rand = new Random();
+        int limit_a = 20;
+        int a = times<limit_a?times:limit_a;
+        times -= times%a;
+        for (int i = 0; i <times/a ; i++)
+        {
+            List<Task<int[]>> tasks = new List<Task<int[]>>();
+            for (int j = 0; j < a; j++)
+            {
+                tasks.Add(Task.Run(() =>
+                {
+                    Thread.Sleep(j);
+                    string[] str = new string[2];
+                    str[0] = rand.Next().ToString();
+                    tmp += "+++"+str[0];
+                    str[1] = path_log;
+                    return Main_old(str,type_solver);
+                })) ;
+            }
+            Task.WaitAll(tasks.ToArray());
+            for (int j = 0; j < tasks.Count(); j++)
+            {
+                log_score[i*limit_a+j] = tasks[j].Result[0];
+                log_count[i*limit_a+j] = tasks[j].Result[1];
+            }
+            Console.WriteLine(tmp);
+        }
+        
+
+
+
+        end =DateTime.Now;
+        int count = log_score.Count(x => x == 2048);
+        int sum_count=0;
+        double accuracy = (double)count/times;
+        Console.WriteLine("精度 : "+accuracy);
+
+        using (FileStream fs = File.Create("./"+path_result));
+        using (StreamWriter w=new StreamWriter("./"+path_result,true, Encoding.GetEncoding("Shift_JIS")))
+        {
+            w.WriteLine("**************************\n");
+            for (int i = 0; i < log_score.Length; i++)
+            {
+                w.WriteLine($"  score : {log_score[i]} count : {log_count[i]} ");
+                if (log_score[i]==2048)sum_count += log_count[i];
+            }
+            string average_count= ""+(double)sum_count / count;
+            w.WriteLine("\n**************************\n");
+            w.WriteLine("depth : 8");
+            w.WriteLine("trials : 1500");
+            w.WriteLine("試行回数 : " + times);
+            w.WriteLine("精度 : " + accuracy);
+            w.WriteLine("平均手数 （成功）: " + (double.TryParse( average_count,out double result)?average_count:"no data"));
+            w.WriteLine("平均所要時間（全部） : " +(end-start).TotalMinutes/times+"分");
+            w.Close();
+
+            openFile(path_result);
+        }
+
+        void openFile(string path)
+        {
+            Process ps = new Process();
+            ps.StartInfo.FileName = path;
+            ps.Start();
+        }
+    }
+}
